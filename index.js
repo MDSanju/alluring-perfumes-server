04461b4cc6536dcdbb71d89994a8024ed084daec
MD Sanju <mdsanju387@gmail.com>
2022-06-24 12:08:55 +0600

    payment data GET

--- index.js
+++ index.js
@@ -171,7 +171,7 @@ async function run() {
       });
 
       // GET API for payment
-      app.get("/orders/:id", async (req, res) => {
+      app.get("/orders/pay/:id", async (req, res) => {
         const id = req.params.id;
         const query = { _id: ObjectId(id) };
         const order = await ordersCollection.findOne(query);
